eb8683a2987ecc3ec13c90c38e51656523095c9c
MateusAmaroDaSilva <ericaeandre44@gmaill.com>
2025-08-19 16:49:46 -0300

    O treinar.py baixa os embeddings do Supabase e atualiza o labels.json com os IDs dos usuários.

--- treinar.py
+++ treinar.py
@@ -5,7 +5,7 @@ import json
 
 # Configurações Supabase
 SUPABASE_URL = "https://bngwnknyxmhkeesoeizb.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJuZ3dua255eG1oa2Vlc29laXpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1MzUwMDcsImV4cCI6MjA3MTExMTAwN30.MVVHAuicG_pkv0OR1h3HEwI-gx7d5hYoqX-xrK17B_U"
+SUPABASE_KEY = "SUA_CHAVE_AQUI"
 BUCKET_NAME = "faces"
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
